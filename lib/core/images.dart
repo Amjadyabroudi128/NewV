0b18ef83c2f8a64e72f5d4a187e02adf4e0a431e
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2026-02-13 18:48:52 +0000

    required image

--- lib/core/images.dart
+++ lib/core/images.dart
@@ -3,13 +3,13 @@ import 'package:flutter/material.dart';
 class MyImage extends StatelessWidget {
   final double? myHeight;
   final double? myWidth;
-  final String? myImage;
+  final String myImage;
   const MyImage({super.key, this.myHeight, this.myWidth, this.myImage});
 
   @override
   Widget build(BuildContext context) {
     return Image.asset(
-      myImage!,
+      myImage,
       height: myHeight,
       width: myWidth,
     );
