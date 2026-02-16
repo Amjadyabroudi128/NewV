147f960b61069fdae27dbb3fa1b0ef3493b215ec
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2026-02-16 16:56:10 +0000

    remove title

--- lib/main.dart
+++ lib/main.dart
@@ -14,14 +14,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
+  const MyHomePage({super.key,});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
