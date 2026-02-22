085389fb6e024465c91d9d7d51fcbd40ddb67ba8
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2026-02-22 14:47:10 +0000

    textStyles.dart yes

--- lib/core/textStyles.dart
+++ lib/core/textStyles.dart
@@ -3,4 +3,10 @@ import 'package:flutter/material.dart';
 import 'myColors.dart';
 class myTstyles {
   static TextStyle noMessage = TextStyle(color: MyColors.noMessage);
+  static TextStyle yes(BuildContext context, Color color, double fontSize, FontWeight fontWeight){
+    return TextStyle(
+        color: color,
+        fontSize: fontSize
+    );
+  }
 }
